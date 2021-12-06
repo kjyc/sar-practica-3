d2a35c4f2b6213c59a8e5c427f8860835eb9053a
IvanIbz0 <70901756+IvanIbz0@users.noreply.github.com>
2021-12-06 11:40:43 +0100

    Add files via upload

--- recipes.php
+++ recipes.php
@@ -1,3 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Recetas</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="resources/css/recipes.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/global.css">
+        <script type="text/javascript" src="./resources/js/recipes.js"></script>
+    </head>
+    <body>
+        <h1>Receitas Para Sua Mae</h1>
+        <div class="navbar">
+          <a href="https://receitaparasuamae.000webhostapp.com/recipes.php">Inicio</a>
+          <div class="dropdown">
+            <button class="dropbtn">Gestionar Recetas 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="#">Nueva Receta</a>
+              <a href="#">Eliminar Receta</a>
+            </div>
+          </div>
+          <a class="signout" href="./resources/php/signout.php">Cerrar Sesión</a>
+        </div>
+
 <?php
 $servername = "localhost";
 $username = "id18030377_root";
@@ -12,20 +37,41 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
 
-$sql = "SELECT id, autor, nombre, preparacion FROM receta";
+$sql = "SELECT * FROM receta NATURAL JOIN ingredientes";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
+  $lastest_id = -1;
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Autor: " . $row["autor"]. " " ."Nombre:" . $row["nombre"]. "Preparacion". $row["preparacion"] . "<br>";
+    if($latest_id != $row['id']){
+        echo('</div>');
+        echo('<div class="receta" id="'.$row['id'].'">');
+          echo('<div class="cabecera_receta">');
+            echo('<div class="imgBox"><img src="./resources/images/'.$row['id'].'.jpg"><br></div>');
+            echo('<span class="nombre">'. $row["nombre"].'</span><br>');
+            echo('<span class="autor">'.'Autor: '. $row["autor"].'</span>');
+          echo('</div>');
+    }
+     if($latest_id != $row['id']){   
+        echo('<form>
+                <input type="button" value="Ver Receta" class="btnReceta" onclick=mostrarReceta('.$row['id'].')>
+        </form>');    
+        echo('</div>');
+     }
+    
+    $latest_id = $row['id'];
   }
+  echo('</div>');
 } else {
   echo "0 results";
 }
 
+
 mysqli_close($conn); 
 
 ?>
+    </body>
+</html>
+
